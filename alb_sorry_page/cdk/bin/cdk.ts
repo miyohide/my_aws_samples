c0e1d6e38f6e026adb88eb5e057d003654d7d44d
Hidenori Miyoshi <miyohide@gmail.com>
2025-08-11 15:05:41 +0900

    タグを付与

--- alb_sorry_page/cdk/bin/cdk.ts
+++ alb_sorry_page/cdk/bin/cdk.ts
@@ -4,3 +4,8 @@ import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack', {});
+
+cdk.Tags.of(app).add('Creator', 'CDK');
+// スタック作成時の日付
+const createdDate = new Date().toISOString().split('T')[0];
+cdk.Tags.of(app).add('CreatedDate', createdDate);
